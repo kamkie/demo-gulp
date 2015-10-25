9f1b00055bf4e423570e3f32cda67a9926ecf32e
Kamil Kiewisz <kamkie@outlook.com>
2015-10-25 22:25:36 +0100

    improvements in build system

--- gulpfile.js
+++ gulpfile.js
@@ -7,50 +7,50 @@ var run = require('gulp-run');
 var del = require('del');
 
 var paths = {
-  scripts: ['src/typescript/*.ts'],
-  images: 'src/img/**/*'
+    scripts: ['src/typescript/*.ts'],
+    images: 'src/img/**/*'
 };
 
 gulp.task('clean', function (cb) {
-  del(['build', 'dist'], cb);
+    del(['build', 'dist'], cb);
 });
 
 gulp.task('run', ['scripts'], function () {
-  var node = new run.Command('node build/js/app.js');
-  node.exec();
-})
+    var node = new run.Command('node build/js/app.js');
+    node.exec();
+});
 
 gulp.task('scripts', [], function () {
-  return gulp.src(paths.scripts)
-    .pipe(typescript({
-      sourceMap: true,
-      declaration: true,
-      outDir: 'build/sourcemaps/'
-    }))
-    .pipe(gulp.dest('build/js'));
+    return gulp.src(paths.scripts)
+        .pipe(typescript({
+            sourceMap: true,
+            declaration: true,
+            outDir: 'build/sourcemaps/'
+        }))
+        .pipe(gulp.dest('build/js'));
 });
 
 // Copy all static images 
 gulp.task('images', [], function () {
-  return gulp.src(paths.images)
-  // Pass in options to the task 
-    .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(gulp.dest('build/img'));
+    return gulp.src(paths.images)
+        // Pass in options to the task
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('build', ['images'], function () {
-  // Minify and copy all JavaScript (except vendor scripts) 
-  // with sourcemaps all the way down 
-  return gulp.src(paths.scripts)
-    .pipe(typescript())
-    .pipe(uglify())
-    .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('dist/js'));
+    // Minify and copy all JavaScript (except vendor scripts)
+    // with sourcemaps all the way down
+    return gulp.src(paths.scripts)
+        .pipe(typescript())
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.images, ['images']);
 });
 
-gulp.task('default', ['clean', 'scripts', 'images']);
+gulp.task('default', ['scripts', 'images']);
--- src/typescript/app.ts
+++ src/typescript/app.ts
@@ -1,13 +1,25 @@
 console.log("--- start ---");
 
 class Startup {
-    public static main(): number {
+
+    private name;
+
+    constructor(name:String) {
+        this.name = name;
+    }
+
+    public static main():number {
         console.log('Hello World');
         return 0;
     }
+
+    public hello():void {
+        console.log('Hello ' + this.name);
+    }
 }
 
-var siema = new Startup();
+var hello = new Startup('world');
+hello.hello();
 Startup.main();
 
 console.log("--- stop ---");
